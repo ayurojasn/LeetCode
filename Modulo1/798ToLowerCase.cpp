a2a633ef0fa586c46925e834de7650799b44cc6b
Juliana Rojas Naranjo <45430896+ayurojasn@users.noreply.github.com>
2020-10-10 11:47:32 -0500

    Solution

--- Modulo1/798ToLowerCase.cpp
+++ Modulo1/798ToLowerCase.cpp
@@ -1,19 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     string word;
     cin>>word;
 
-    for (int i=0; i<word[i]; i++)
-    {
-        if(word[i]>='A' && word[i]<='Z')
-        {
-
+    for (int i=0; i<word[i]; i++){
+        if(word[i]>='A' && word[i]<='Z'){
             word[i]+=32;
         }
-
     }
 
     cout<<word;
