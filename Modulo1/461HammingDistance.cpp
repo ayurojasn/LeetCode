fa88ef31ebaa619becfcae116617dfd9b3abf378
Juliana Rojas Naranjo <45430896+ayurojasn@users.noreply.github.com>
2020-10-10 11:46:12 -0500

    Solution

--- Modulo1/461HammingDistance.cpp
+++ Modulo1/461HammingDistance.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 int main(){
 
-int x,y,res, cont=0;
-cin>>x;
-cin>>y;
-res=x^y;
+  int x,y,res, cont=0;
+  cin>>x;
+  cin>>y;
+  res=x^y;
 
-
-while(res!=0){
+  while(res!=0){
     res&=(res-1);
     cont++;
-}
+  }
 
-cout<<cont;
+  cout<<cont;
 
-return 0;
+  return 0; 
 }
